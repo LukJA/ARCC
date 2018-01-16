de46890e0140238731a0df9da0b24094439dda6e
Luke Andrews <lukejandrews@hotmail.co.uk>
2018-01-16 20:37:13 +0000

    fixed naming so the assembler functions

--- ARCC/Generated Files/MainPage.g.h
+++ ARCC/Generated Files/MainPage.g.h
@@ -7,6 +7,24 @@
 //------------------------------------------------------------------------------
 
 
+namespace Windows {
+    namespace UI {
+        namespace Xaml {
+            namespace Controls {
+                ref class Pivot;
+                ref class Frame;
+                ref class RelativePanel;
+                ref class TextBlock;
+                ref class TextBox;
+                ref class StackPanel;
+                ref class Button;
+                ref class ComboBox;
+                ref class ScrollViewer;
+                ref class CheckBox;
+            }
+        }
+    }
+}
 
 namespace ARCC
 {
@@ -23,6 +41,39 @@ namespace ARCC
     private:
         bool _contentLoaded;
     
+        private: ::Windows::UI::Xaml::Controls::Pivot^ rootPivot;
+        private: ::Windows::UI::Xaml::Controls::Frame^ helpFrame;
+        private: ::Windows::UI::Xaml::Controls::RelativePanel^ helpStack;
+        private: ::Windows::UI::Xaml::Controls::TextBlock^ tb1;
+        private: ::Windows::UI::Xaml::Controls::TextBox^ console;
+        private: ::Windows::UI::Xaml::Controls::Frame^ simFrame;
+        private: ::Windows::UI::Xaml::Controls::Frame^ insFrame;
+        private: ::Windows::UI::Xaml::Controls::StackPanel^ globalStack;
+        private: ::Windows::UI::Xaml::Controls::StackPanel^ stackPanel;
+        private: ::Windows::UI::Xaml::Controls::TextBox^ script;
+        private: ::Windows::UI::Xaml::Controls::RelativePanel^ topBar;
+        private: ::Windows::UI::Xaml::Controls::Button^ button;
+        private: ::Windows::UI::Xaml::Controls::Button^ reload;
+        private: ::Windows::UI::Xaml::Controls::ComboBox^ width;
+        private: ::Windows::UI::Xaml::Controls::Button^ generate;
+        private: ::Windows::UI::Xaml::Controls::Button^ Load;
+        private: ::Windows::UI::Xaml::Controls::Frame^ asmFrame;
+        private: ::Windows::UI::Xaml::Controls::RelativePanel^ boqqq;
+        private: ::Windows::UI::Xaml::Controls::RelativePanel^ Tools;
+        private: ::Windows::UI::Xaml::Controls::ScrollViewer^ in;
+        private: ::Windows::UI::Xaml::Controls::ScrollViewer^ LineNo;
+        private: ::Windows::UI::Xaml::Controls::ScrollViewer^ out;
+        private: ::Windows::UI::Xaml::Controls::TextBox^ outT;
+        private: ::Windows::UI::Xaml::Controls::TextBox^ LineNoT;
+        private: ::Windows::UI::Xaml::Controls::TextBox^ inT;
+        private: ::Windows::UI::Xaml::Controls::Button^ ASSLoad;
+        private: ::Windows::UI::Xaml::Controls::Button^ ASSsave;
+        private: ::Windows::UI::Xaml::Controls::Button^ ISLoad;
+        private: ::Windows::UI::Xaml::Controls::TextBox^ Len;
+        private: ::Windows::UI::Xaml::Controls::TextBlock^ fX;
+        private: ::Windows::UI::Xaml::Controls::Button^ ASGen;
+        private: ::Windows::UI::Xaml::Controls::CheckBox^ MIF;
+        private: ::Windows::UI::Xaml::Controls::CheckBox^ BIN;
     };
 }
 
--- ARCC/MainPage.xaml.h
+++ ARCC/MainPage.xaml.h
@@ -4,7 +4,6 @@
 //
 
 #pragma once
-
 #include "MainPage.g.h"
 
 namespace ARCC
@@ -17,5 +16,29 @@ namespace ARCC
 	public:
 		MainPage();
 
+	private:
+		void debugConsole(std::string in);
+		void debugConsole(std::wstring wIn);
+		void debugConsole(Platform::String ^ pIn);
+		void closeClicked(Platform::Object ^ sender, Windows::UI::Xaml::RoutedEventArgs ^ e);
+		void button_Click(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e);
+		void setGen(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e);
+		void dynamicRealloc(Platform::Object ^ sender, Windows::UI::Xaml::RoutedEventArgs^ e);
+		void loadIx(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e);
+		void decodeIx(Platform::String ^ file);
+		void fillLineNo(int len);
+		void openIQ(Platform::String ^ file, Platform::String^ name);
+		void LoadIS(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e);
+		void VC(Platform::Object^ sender, Windows::UI::Xaml::Controls::ScrollViewerViewChangedEventArgs^ e);
+		void OpenAsmFile(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e);
+		void openASM(Platform::String ^ file);
+		void SaveASM(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e);
+		void generateMC(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e);
+		void fileGenOT(std::string name);
+		void Mcheck(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e);
+		void MUcheck(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e);
+		void Bcheck(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e);
+		void BUcheck(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e);
+		void displayErrorBox(Platform::String ^ ErrorCode);
 	};
 }
