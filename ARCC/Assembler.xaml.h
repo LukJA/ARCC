cb025ca1888e2a5a1a1ea93e930ca486e646e845
Luke Andrews <lukejandrews@hotmail.co.uk>
2018-01-22 16:15:22 +0000

    transfered old app to assembler page and updated ux

---
+++ ARCC/Assembler.xaml.h
@@ -0,0 +1,44 @@
+﻿//
+// Assembler.xaml.h
+// Declaration of the Assembler class
+//
+
+#pragma once
+
+#include "Assembler.g.h"
+#include "MainPage.xaml.h"
+
+namespace ARCC
+{
+	/// <summary>
+	/// An empty page that can be used on its own or navigated to within a Frame.
+	/// </summary>
+	[Windows::Foundation::Metadata::WebHostHidden]
+	public ref class Assembler sealed
+	{
+	public:
+		Assembler();
+
+	private:
+		void closeClicked(Platform::Object ^ sender, Windows::UI::Xaml::RoutedEventArgs ^ e);
+		void button_Click(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e);
+		void setGen(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e);
+		void dynamicRealloc(Platform::Object ^ sender, Windows::UI::Xaml::RoutedEventArgs^ e);
+		void loadIx(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e);
+		void decodeIx(Platform::String ^ file);
+		void fillLineNo(int len);
+		void openIQ(Platform::String ^ file, Platform::String^ name);
+		void LoadIS(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e);
+		void VC(Platform::Object^ sender, Windows::UI::Xaml::Controls::ScrollViewerViewChangedEventArgs^ e);
+		void OpenAsmFile(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e);
+		void openASM(Platform::String ^ file);
+		void SaveASM(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e);
+		void generateMC(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e);
+		void fileGenOT(std::string name);
+		void Mcheck(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e);
+		void MUcheck(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e);
+		void Bcheck(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e);
+		void BUcheck(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e);
+		void displayErrorBox(Platform::String ^ ErrorCode);
+	};
+}
